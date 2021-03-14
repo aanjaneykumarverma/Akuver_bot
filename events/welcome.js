70ebef1e2fb26e1e01931f6b12258647cd86508d
Akuver <aanjaneykumar.verma@gmail.com>
2021-03-14 20:47:27 +0530

    Joke command added

--- events/welcome.js
+++ events/welcome.js
@@ -3,9 +3,9 @@ module.exports = {
   execute(member,client){
     const channelId = 820653558209708112; //welcome
     const targetChannelId = 815477746310184962; // rules-and-info
-    const msg = `Welcome aboard <@${member.user.username}>! Please check out ${member.guild.channels.cache.get(targetChannelId).toString()}`
+    const msg = `Welcome aboard <@${member.user.username}>! Please check out ${member.guild.channels.cache.get(targetChannelId).toString()}`;
 
     const channel = member.guild.channels.cache.get(channelId);
     channel.send(msg);
-  }
-}
+  },
+};
