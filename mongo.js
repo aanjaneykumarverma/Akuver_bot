84dad39d07139e7fc76ee50f16beb15ece5e47c9
Akuver <aanjaneykumar.verma@gmail.com>
2021-03-28 20:49:45 +0530

    XP system added

--- mongo.js
+++ mongo.js
@@ -2,9 +2,10 @@ require("dotenv").config();
 const mongoose = require('mongoose');
 const mongoPath = process.env.mongoPath;
 
-module.exports = async() =>
+module.exports = async() => {
     await mongoose.connect(mongoPath,{
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     return mongoose;
+  }
