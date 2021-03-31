c8f47b0141321774bd77054681348faae3098192
Akuver <aanjaneykumar.verma@gmail.com>
2021-03-31 12:22:34 +0530

    polls feature added

---
+++ features/polls.js
@@ -0,0 +1,19 @@
+const channels = ['826698915763060807','825706248783003648'];  //polls, testing
+
+module.exports = (client) => {
+  client.on('message', (message) => {
+    const { channel, content } = message;
+    if (!channels.includes(channel.id)) {
+      return;
+    }
+    const eachLine = content.split('\n');
+
+    for (const line of eachLine) {
+      if (line.includes('=')) {
+        const split = line.split('=');
+        const emoji = split[0].trim();
+        message.react(emoji);
+      }
+    }
+  })
+}
