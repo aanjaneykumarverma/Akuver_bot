ea966e25b74a050bd7103050fb621cdd21285659
Akuver <aanjaneykumar.verma@gmail.com>
2021-04-04 23:41:16 +0530

    Currency issue fix #1

--- features/currency.js
+++ features/currency.js
@@ -8,7 +8,7 @@ module.exports = (client) => {};
 module.exports.addCoins = async (guildId, userId, coins) => {
   return await mongo().then(async (mongoose) =>{
     try{
-      const result = await profileSchema.findOneAndUpdate({
+      await profileSchema.findOneAndUpdate({
         guildId,
         userId,
       },{
@@ -20,11 +20,13 @@ module.exports.addCoins = async (guildId, userId, coins) => {
       },{
         upsert: true,
         new: true,
+      }).then((result)=>{
+        coinsCache[`${guildId}-${userId}`] = result.coins;
+        mongoose.connection.close();
+        return result.coins;
       });
-      coinsCache[`${guildId}-${userId}`] = result.coins;
-      return result.coins;
-    } finally{
-      mongoose.connection.close();
+    } catch(err){
+      console.log(err);
     }
   });
 };
@@ -39,21 +41,23 @@ module.exports.getCoins = async(guildId, userId) => {
       const result = await profileSchema.findOne({
         guildId,
         userId,
+      }).then(async ()=>{
+        let coins = 0;
+        if(result){
+          coins = result.coins;
+        } else{
+          await new profileSchema({
+            guildId: guildId,
+            userId: userId,
+            coins: coins,
+          }).save();
+        }
+        coinsCache[`${guildId}-${userId}`] = coins;
+        mongoose.collection().close();
+        return coins;
       });
-      let coins = 0;
-      if(result){
-        coins = result.coins;
-      } else{
-        await new profileSchema({
-          guildId: guildId,
-          userId: userId,
-          coins: coins,
-        }).save();
-      }
-      coinsCache[`${guildId}-${userId}`] = coins;
-      return coins;
-    } finally{
-      mongoose.connection.close();
+    } catch (err){
+      console.log(err);
     }
   });
 };
@@ -61,13 +65,14 @@ module.exports.getCoins = async(guildId, userId) => {
 module.exports.sort = async(guildId,max) => {
   return await mongo().then(async (mongoose)=>{
     try{
-      const all = await profileSchema.find()
+        await profileSchema.find()
        .sort({'coins':'desc'})
-       .limit(max);
-       return all;
-   }
-    finally{
-      mongoose.connection.close();
+       .limit(max).then((response)=>{
+         mongoose.connection.close();
+         return response;
+       });
+   }catch(err){
+      console.log(err);
     }
   });
 };
