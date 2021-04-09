47256e07d15a11f1f4185935eb38fc2abeee302c
Akuver <aanjaneykumar.verma@gmail.com>
2021-04-10 00:13:27 +0530

    Scheduled msg issue fix#1

--- commands/utility/schedule.js
+++ commands/utility/schedule.js
@@ -1,5 +1,6 @@
 const momentTimezone = require('moment-timezone');
 const {MessageCollector} = require('discord.js');
+const mongo = require('../../util/mongo.js');
 const scheduledSchema = require('../../schemas/scheduled-schema.js');
 
 module.exports = {
@@ -8,31 +9,6 @@ module.exports = {
   usage: '<Channel tag> <YYYY/MM/DD> <HH:MM> <"AM" or "PM"> <Timezone>',
   permissions: 'ADMINISTRATOR',
   guildOnly: true,
-  init: (client) => {
-    const checkForPosts = async () => {
-      const query = {
-        date: {
-          $lte: Date.now(),
-        },
-      }
-      const results = await scheduledSchema.find(query);
-      for(const post of results){
-        const {guildId, channelId, content} = post;
-        const guild = await client.guilds.fetch(guildId);
-        if(!guild){
-          continue;
-        }
-        const channel = guild.channels.cache.get(channelId);
-        if(!channel){
-          continue;
-        }
-        channel.send(content);
-      }
-      await scheduledSchema.deleteMany(query);
-      setTimeout(checkForPosts, 1000*10);
-    }
-    checkForPosts();
-  },
   async execute(message, args){
     const {mentions, guild, channel } = message;
     const targetChannel = mentions.channels.first();
@@ -71,12 +47,14 @@ module.exports = {
         return;
       }
       message.reply('Your message has been scheduled.');
+      const mongoose = await mongo();
       await new scheduledSchema({
         date: targetDate.format(),
         content: collectedMessage.content,
         guildId: guild.id,
         channelId: targetChannel.id,
       }).save();
+      mongoose.connection.close();
     })
   },
 };
---
+++ features/scheduled.js
@@ -0,0 +1,30 @@
+const mongo = require('../util/mongo.js');
+const scheduledSchema = require('../schemas/scheduled-schema.js');
+
+module.exports = (client) => {
+  const checkForPosts = async () => {
+    const query = {
+      date: {
+        $lte: Date.now(),
+      },
+    }
+    const mongoose = await mongo();
+    const results = await scheduledSchema.find(query);
+    for(const post of results){
+      const {guildId, channelId, content} = post;
+      const guild = await client.guilds.fetch(guildId);
+      if(!guild){
+        continue;
+      }
+      const channel = guild.channels.cache.get(channelId);
+      if(!channel){
+        continue;
+      }
+      channel.send(content);
+    }
+    await scheduledSchema.deleteMany(query);
+    setTimeout(checkForPosts, 1000*10);
+    mongoose.connection.close();
+  }
+  checkForPosts();
+};
