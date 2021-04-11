757a11dfd94182200a7b2d0ad87052cc32db73f4
Akuver <aanjaneykumar.verma@gmail.com>
2021-04-11 17:03:12 +0530

    scheduled msg fix #6

--- features/scheduled.js
+++ features/scheduled.js
@@ -23,7 +23,7 @@ module.exports = (client) => {
     }
     console.log('hola');
     await scheduledSchema.deleteMany(query);
-    mongoose.connection.close();
+    //mongoose.connection.close();
     setTimeout(checkForPosts, 1000*10);
   };
   checkForPosts();
