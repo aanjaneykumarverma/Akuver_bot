f917f637e6ee78a36ec26b90965fab0143ce260f
Akuver <aanjaneykumar.verma@gmail.com>
2021-07-06 23:23:28 +0530

    Give Role command added

---
+++ commands/moderation/giverole.js
@@ -0,0 +1,25 @@
+module.exports = {
+  name: 'giverole',
+  description: 'Give a role to the specified user',
+  usage: ' @Member <Role>',
+  permissions: 'ADMINISTRATOR',
+  guildOnly: true,
+  execute(message, args) {
+    const targetUser = message.mentions.users.first();
+    if (!targetUser) {
+      return message.reply('Please specify someone to give a role to. ');
+    }
+    args.shift();
+    const roleName = args.join(' ');
+    const { guild } = message;
+    const role = guild.roles.cache.find((role) => {
+      return role.name === roleName;
+    });
+    if (!role) {
+      return message.reply('Please mention a valid role!');
+    }
+    const member = guild.members.cache.get(targetUser.id);
+    member.roles.add(role);
+    message.reply('Successfully assigned the role to the mentioned user.');
+  },
+};
